5f6df8cd36a1e415b7b6e23e8e9f90049bb72530
vedeneev.t <159995vedeneevt@users.no-reply.gitverse.ru>
2026-04-24 15:41:35 +0300

    upd1

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
